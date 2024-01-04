6771eb30a8621222ff7a8c322e619a82e51dda9d
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2024-01-04 20:38:16 +0100

    add new 3 projects

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,6 @@
 #!/usr/bin/env ruby
-# A regular expression that is matches a given pattern
-puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
+
+from = ARGV[0].scan(/from:(.*?)\]/)
+to = ARGV[0].scan(/to:(.*?)\]/)
+flags = ARGV[0].scan(/flags:(.*?)\]/)
+puts [from, to, flags].join(',')
--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,3 +1,2 @@
 #!/usr/bin/env ruby
-# A regular expression that is matches 10 digit phone number
-puts ARGV[0].scan(/^[0-9]{10}$/).join
+puts ARGV[0].scan(/^\d{10}$/).join
